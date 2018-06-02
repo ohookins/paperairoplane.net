bba49661c1fe5a419912de5ce619706443ba95d7
Oliver Hookins <ohookins@gmail.com>
2018-06-02 20:30:49 +0200

    Align and size pagination links better.

--- src/templates/index.js
+++ src/templates/index.js
@@ -31,12 +31,12 @@ class IndexPage extends React.Component {
                                 <title>chevronLeft icon</title>
                                 <path d="M20 1 L24 5 L14 16 L24 27 L20 31 L6 16 z"></path>
                             </svg>
-                            <span className="ph2">Previous</span>
+                            <span className="ph2 f4 v-btm">Previous</span>
                         </Link>}
                     </div>
                     <div className="nextPost pagination ph2">
                         { nextPath && <Link to={nextPath}>
-                            <span className="ph2">Next</span>
+                            <span className="ph2 f4 v-btm">Next</span>
                             <svg className="w1" data-icon={"chevronRight"} viewBox={"0 0 32 32"} style={{fill:`currentcolor`}}>
                                 <title>chevronRight icon</title>
                                 <path d="M12 1 L26 16 L12 31 L8 27 L18 16 L8 5 z"></path>
