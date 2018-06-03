009fb17d397a6728f16dd1f41e9d4745a4b8badf
Oliver Hookins <ohookins@gmail.com>
2018-06-03 12:31:21 +0200

    Add static about page.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link';
 
 const Footer = () => (
     <div className="cabin center mw6 flex justify-center">
-      <div className="ph2 pt4">© 2010-2018 Oliver Hookins</div>
+      <div className="ph2 pt4">© 2010-2018 Oliver Hookins and Angela Collins</div>
     </div>
 );
   
---
+++ src/pages/about.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import oliver from './oliver.jpg'
+import angela from './angela.jpg'
+
+const AboutPage = () => (
+  <div className="flex flex-column">
+    <h2>About the authors...</h2>
+    <div>
+        <img className="fl pa2" src={oliver} />
+        <p className="f5 pl1">
+        You can find Oliver
+        on <a href="https://www.linkedin.com/in/oliverhookins/" className="b"> LinkedIn </a>
+        and <a href="https://twitter.com/OliverHookins" className="b">Twitter</a>.
+        </p>
+    </div>
+    <div>
+        <img className="fl pa2" src={angela} />
+        <p className="f5 pl1">
+        You can find Angela on <a href="https://www.facebook.com/angelacollinsyogaberlin/" className="b">Facebook</a>,
+        <a href="https://twitter.com/Yogawithangela" className="b"> Twitter </a>
+        and <a href="https://www.instagram.com/angelacollinsyoga/" className="b">Instagram</a>.
+        </p>
+    </div>
+  </div>
+)
+
+export default AboutPage
