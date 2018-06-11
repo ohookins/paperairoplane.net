b1c0305b4d24baac98612492d09ba80bf3d2ceaa
Oliver Hookins <ohookins@gmail.com>
2018-06-11 20:31:41 +0200

    Add Disqus comments to posts.

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,17 +2,26 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import PublishLine from "../components/PublishLine";
 import TagCluster from "../components/TagCluster";
+import { DiscussionEmbed } from "disqus-react";
 
 class BlogPost extends Component {
   render() {
     const {
       title,
+      slug,
       body,
       author,
       published,
       category,
       tags
     } = this.props.data.contentfulPost;
+    const disqusShortname = "paperairoplane";
+    const disqusConfig = {
+        url: "https://paperairoplane.net/posts/" + slug,
+        identifier: slug,
+        title: title,
+    };
+
     return (
       <div>
         <h1 className="f4 f4-m f3-ns">{title}</h1>
@@ -25,6 +34,7 @@ class BlogPost extends Component {
         <div
           dangerouslySetInnerHTML={{ __html: body.childMarkdownRemark.html }}
         />
+        <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
       </div>
     );
   }
