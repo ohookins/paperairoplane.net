d939f489d6c01165192a974caa92fbefd23af5f6
Oliver Hookins <ohookins@gmail.com>
2018-06-11 18:31:23 +0200

    Add RSS feed support.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'paperairoplane.net',
+    title: 'adrift on a cosmic ocean',
+    siteUrl: 'https://paperairoplane.net',
+    description: 'Writings on various topics (mostly technical) from Oliver Hookins and Angela Collins',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -10,6 +12,66 @@ module.exports = {
       options: {
         spaceId: process.env.SPACE_ID,
         accessToken: process.env.ACCESS_TOKEN
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+		`,
+		feeds: [
+          {
+            serialize: ({ query: { site, allContentfulPost } }) => {
+              return allContentfulPost.edges.map(edge => {
+                return Object.assign({}, edge.node, {
+                  title: edge.node.title,
+                  description: edge.node.body.childMarkdownRemark.excerpt,
+                  author: edge.node.author.firstName + " " + edge.node.author.lastName,
+                  date: edge.node.published,
+                  url: site.siteMetadata.siteUrl + '/posts/' + edge.node.slug,
+                  guid: site.siteMetadata.siteUrl + '/posts/' + edge.node.slug,
+                });
+              });
+            },
+			query: `
+              {
+                allContentfulPost (limit: 1000) {
+                    edges {
+                        node {
+                            id
+                            slug
+                            title
+                            published
+                            category {
+                                realname
+                            }
+                            author {
+                                firstName
+                                lastName
+                            }
+                            body {
+                                childMarkdownRemark {
+                                    excerpt(pruneLength: 400)
+                                }
+                            }
+                        }
+                    }
+                }
+              }
+			`,
+			output: "/rss.xml",
+		  },
+		],
       }
     }
   ],
